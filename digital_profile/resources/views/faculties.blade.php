c58666c34c92e5c0d4ae405beb9fd650d1db7a05
Kатерина <shtalina03@gmail.com>
2023-04-09 22:04:23 +0700

    update

--- digital_profile/resources/views/faculties.blade.php
+++ digital_profile/resources/views/faculties.blade.php
@@ -6,45 +6,39 @@
     <div class="container" align="center">
         <h2>Факультеты, институты</h2>
         <br>
-        <div class="container">
-            <div class="accordion mb-2" id="accordionExample">
+        <div class="container" >
+            @foreach($data as $el)
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne{{$el->fak_id}}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$el->fak_id}}" aria-expanded="false" aria-controls="flush-collapseOne{{$el->fak_id}}">
+                                {{$el->name}}
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne{{$el->fak_id}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$el->fak_id}}" data-bs-parent="#accordionFlushExample">
 
-
-                <div class="accordion-item">
-                    @foreach($data as $el)
-                    <h2 class="accordion-header" id="flush-headingOne">
-
-                        <button  class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                                 aria-expanded="true" aria-controls="collapseOne">
-                           {{ $el->name }}
-                        </button>
-
-                    </h2>
-
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                         data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            @foreach($el->groups as $groups)
-                            <div class="row">
-                                <div class="col-sm-2">
-                                    <div class="col-sm-12 p-2">{{ $groups->course }}</div>
-
+                            <div class="accordion-body">
+                                <div class="row">
                                     @foreach($el->groups as $groups)
-                        <a class="btn btn-primary" href="#" role="button" id="get-users">{{ $groups->gruppa }}</a><div id="result"></div>
-                            @endforeach
+                                    <div class="col-sm-2">
+                                        <div class="col-sm-12 p-2" >{{$groups->course}}</div>
+                                        <div class="list-group p-1">
+                                            @foreach($el->groups as $gr)
+                                            <a href="#" class="list-group-item ">{{$gr->gruppa}}</a>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    @endforeach
+
                                 </div>
                             </div>
-                            @endforeach
+
                         </div>
                     </div>
                     @endforeach
-                    </div>
 
             </div>
-        </div>
-    </div>
 
-                </div>
 
             </div>
         </div>
